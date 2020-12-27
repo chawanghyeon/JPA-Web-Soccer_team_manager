3eb97082fa055fefe04eb1de3317bb4588d151ce
YunhyeJo <73386460+YUNHYE-0107@users.noreply.github.com>
2020-12-27 21:37:02 +0900

    sql

---
+++ sql/soccermagDDL.sql
@@ -0,0 +1,94 @@
+DROP TABLE players CASCADE CONSTRAINTS;
+
+DROP TABLE team CASCADE CONSTRAINTS;
+
+DROP TABLE managers CASCADE CONSTRAINTS;
+
+DROP TABLE medical_staff CASCADE CONSTRAINTS;
+
+DROP TABLE trainers CASCADE CONSTRAINTS;
+
+DROP TABLE logins CASCADE CONSTRAINTS;
+
+
+CREATE TABLE team(
+	t_name	         varchar2(25)    NOT NULL,
+	userid	         varchar2(50)	NOT NULL
+);
+
+
+CREATE TABLE  players (
+	p_number           number(20)		NOT NULL,
+	t_name             varchar2(25)		NOT NULL,
+	p_name 	           varchar2(50)		NULL,
+	p_age	           number(20)		NULL,
+    p_position         varchar2(50)		NULL
+);
+
+
+CREATE TABLE  managers (
+	 m_number	 number(20)		    NOT NULL,
+	 t_name	     varchar2(25)		NOT NULL,
+	 m_name  	 varchar2(50)		NULL,
+	 m_age	     number(20)		    NULL,
+	 m_position	 varchar2(50)		NULL
+);
+
+
+
+CREATE TABLE medical_staff (
+	d_number	      number(20)		    NOT NULL,
+	t_name	          varchar2(25)		NOT NULL,
+	d_name	          varchar2(50)		NULL,
+	d_age	          number(20)		    NULL,
+	d_position	      varchar2(50)		    NULL
+);
+
+
+
+CREATE TABLE trainers (
+	tr_number	 number(20)		    NOT NULL,
+	t_name	     varchar2(25)		NOT NULL,
+	tr_name	     varchar2(50)		NULL,
+	tr_age	     number(20)		    NULL,
+	tr_position	 varchar2(50)		    NULL
+);
+
+
+
+CREATE TABLE logins (
+	userid	           varchar2(50)	      NOT NULL,
+	userpw	           varchar2(50)	      NOT NULL
+);
+
+
+ALTER TABLE logins ADD CONSTRAINT PK_LOGINS PRIMARY KEY (userid);
+
+ALTER TABLE players ADD CONSTRAINT PK_PLAYERS PRIMARY KEY (p_number);
+
+ALTER TABLE team ADD CONSTRAINT PK_TEAM PRIMARY KEY (t_name);
+
+ALTER TABLE team ADD CONSTRAINT FK_logins_TO_team_1 FOREIGN KEY (userid)
+REFERENCES logins (userid);
+
+ALTER TABLE managers ADD CONSTRAINT PK_MANAGERS PRIMARY KEY (m_number);
+
+ALTER TABLE medical_staff ADD CONSTRAINT PK_MEDICAL_STAFF PRIMARY KEY (d_number);
+
+ALTER TABLE trainers ADD CONSTRAINT PK_TRAINERS PRIMARY KEY (tr_number);
+
+ALTER TABLE players ADD CONSTRAINT FK_team_TO_players_1 FOREIGN KEY (t_name)
+REFERENCES team (t_name);
+
+ALTER TABLE managers ADD CONSTRAINT FK_team_TO_managers_1 FOREIGN KEY (t_name)
+REFERENCES team (t_name);
+
+ALTER TABLE medical_staff ADD CONSTRAINT FK_team_TO_medical_staff_1 FOREIGN KEY (t_name)
+REFERENCES team (t_name);
+
+ALTER TABLE trainers ADD CONSTRAINT FK_team_TO_trainers_1 FOREIGN KEY (t_name)
+REFERENCES team (t_name);
+
+
+
+
---
+++ sql/soccermagDML.sql
@@ -0,0 +1,60 @@
+insert into logins values ('cwh', '1234');
+insert into logins values ('yoon1234', '4567');
+
+insert into team values ('토트넘', 'cwh');
+insert into team values ('유벤투스', 'yoon1234');
+
+
+insert into players values(1,'토트넘','권오민',23,'골키퍼');	
+insert into players values(2,'토트넘','권희성',25,'수비수');	
+insert into players values(3,'토트넘','김민건',27,'수비수');	
+insert into players values(4,'토트넘','김성호',29,'수비수');	
+insert into players values(5,'토트넘','김연식',28,'수비수');	
+insert into players values(6,'토트넘','김재웅',27,'공격수');	
+insert into players values(7,'토트넘','김준형',30,'공격수');	
+insert into players values(8,'토트넘','김창훈',25,'공격수');	
+insert into players values(9,'토트넘','김혜경',24,'미드필더');	
+insert into players values(10,'토트넘','김혜성',21,'미드필더');	
+insert into players values(11,'토트넘','박다영',22,'미드필더');	
+insert into players values(12,'유벤투스','염아정',24,'골키퍼');	
+insert into players values(13,'유벤투스','김연지',26,'수비수');	
+insert into players values(14,'유벤투스','박민영',28,'수비수');	
+insert into players values(15,'유벤투스','고은비',27,'수비수');	
+insert into players values(16,'유벤투스','장종욱',25,'수비수');	
+insert into players values(17,'유벤투스','이정민',24,'공격수');	
+insert into players values(18,'유벤투스','조윤혜',26,'공격수');	
+insert into players values(19,'유벤투스','최지수',24,'공격수');	
+insert into players values(20,'유벤투스','최지원',27,'미드필더');	
+insert into players values(21,'유벤투스','최태열',26,'미드필더');	
+insert into players values(22,'유벤투스','양호준',25,'미드필더');	
+	
+				
+
+
+insert into trainers values(301,'토트넘','배수지',34,'재활');
+insert into trainers values(302,'토트넘','김태연',50,'재활');
+insert into trainers values(303,'유벤투스','한소희',40,'재활');
+insert into trainers values(304,'토트넘','김준면',30,'PT');
+insert into trainers values(305,'유벤투스','김선호',36,'PT');
+insert into trainers values(306,'유벤투스','남주혁',37,'PT');	
+
+
+
+insert into managers values (100, '토트넘', 'José Mourinho', 60, 'Manager');
+insert into managers values (101, '토트넘', 'João Sacramento', 50, 'Coach');
+insert into managers values (102, '토트넘', 'Nuno Santos', 40, 'Coach');
+insert into managers values (103, '유벤투스', 'Ledley King', 46, 'Coach');
+insert into managers values (104, '유벤투스', 'Carlos Lalin', 48, 'Coach');
+insert into managers values (105, '유벤투스', 'Giovanni Cerra', 57, 'Coach');
+				
+
+
+insert into medical_staff values (200, '토트넘', '차의사', 30, '정신과');			
+insert into medical_staff values (201, '토트넘', '조의사', 40, '정형외과');			
+insert into medical_staff values (202, '토트넘', '김의사', 32, '내과');			
+insert into medical_staff values (203, '유벤투스', '박의사', 43, '정형외과');			
+insert into medical_staff values (204, '유벤투스', '이의사', 50, '정형외과');			
+insert into medical_staff values (205, '유벤투스', '최의사', 53, '정형외과');			
+					
+				
+commit;
